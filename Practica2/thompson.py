9c002f5290a546e5800d2839ff449237af1a8bf9
Karsa19 <38262267+Karsa19@users.noreply.github.com>
2021-01-24 21:44:21 -0600

    Nuevos métodos en practicas 2 y 3

--- Practica2/thompson.py
+++ Practica2/thompson.py
@@ -2,4 +2,23 @@
 
 class Thompson(object):
 
-    pass
+    def __init__(self):
+        pass
+
+
+    def exp_epsilon(self):
+        pass
+
+    def exp_a(self):
+        pass
+
+    def exp_or(self):
+        pass
+
+    def exp_concatenacion(self):
+        pass
+
+    def cerradura_estrella(self):
+        pass
+
+
--- Practica3/Conjuntos.py
+++ Practica3/Conjuntos.py
@@ -4,8 +4,20 @@ class Conjuntos(object):
 
     def hacer_Subconjuntos(self, afn):
 
+        while ( hay un estado sin marcar T en Destados ): 
+            marcar T
+            for ( cada símbolo de entrada a ): 
+                U = cerradura_Epsilon(mover(T, a))
+                if ( U no está en Destados ):
+                    agregar U como estado sin marcar a Destados
+                Dtran[T, a] = U
+ 
+
+
 
     def cerradura_Epsilon():
+        pass
 
     
     def mover():
+        pass
